ae7f61661c22fc4f8d5111cb91290668050b928c
Jordan Murphy <jmurphy6895@gmail.com>
2025-08-02 13:24:30 +0300

    fix: gating problematic packages on nightly CI, remove diffractor, update CI to 1.10 instead of 1.6

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,28 +7,44 @@ using Scratch
 using SpaceIndices
 
 using DifferentiationInterface
-using FiniteDiff, ForwardDiff, Diffractor, Enzyme, PolyesterForwardDiff, Zygote
+using FiniteDiff, ForwardDiff, PolyesterForwardDiff, Zygote
 
 if isempty(VERSION.prerelease)
-    using Mooncake
+    # Add Mooncake and Enzyme to the project if not the nightly version
+    # Adding them via the Project.toml isn't working because it tries to compile them before reaching the gating
+    using Pkg
+    Pkg.add("Mooncake")
+    Pkg.add("Enzyme")
+    Pkg.add("JET")
+    Pkg.add("AllocCheck")
+
+    # Test with Mooncake and Enzyme along with the other backends
+    using Mooncake, Enzyme
     const _BACKENDS = (
         ("ForwardDiff", AutoForwardDiff()),
         ("Enzyme", AutoEnzyme()),
         ("Mooncake", AutoMooncake(;config=nothing)),
         ("PolyesterForwardDiff", AutoPolyesterForwardDiff()),
     )
+
+    using JET
+    using AllocCheck
+
+    @testset "Performance" verbose = true begin
+        include("./performance.jl")
+    end
 else
     @warn "Mooncake.jl not guaranteed to work on julia-nightly, skipping tests"
     const _BACKENDS = (
         ("ForwardDiff", AutoForwardDiff()),
-        ("Enzyme", AutoEnzyme()),
         ("PolyesterForwardDiff", AutoPolyesterForwardDiff()),
     )
+
+    @warn "JET and AllocCheck not guaranteed to work on julia-nightly, skipping tests"
+
 end
 
-using AllocCheck
 using Aqua
-using JET
 
 const _INDICES = [
     :F10obs
@@ -71,10 +87,10 @@ end
     include("./interpolations.jl")
 end
 
-@testset "Performance" verbose = true begin
-    include("./performance.jl")
-end
-
 @testset "Automatic Differentiation" verbose = true begin
     include("./differentiability.jl")
+end
+
+@testset "Aqua.jl" begin
+    Aqua.test_all(SpaceIndices; ambiguities=(recursive = false), deps_compat=(check_extras = false))
 end
